0a630bd349f999de320f3a094f7f239cfb3be0af
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-28 12:50:06 -0700

    lab 4-7 Camerino A

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def main():
     Code Your Program here
     ########################################
     """
-
+    lastval = False
+    while True:
+        Value= int(input("input Number"))
+        if lastval is not False and Value <= lastval:
+            numbers.append(lastval)
+        else:
+            lastval = True
+            if Value>lastval:
+             break
+        lastval==Value
     ########################################
     # Do not delete the return statement
     ########################################
